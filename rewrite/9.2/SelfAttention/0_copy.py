36db6d4881e3318eae7a15bec883e4bb93996d4d
Freeze Treat <freezetreat@github.com>
2024-04-07 18:46:52 -0700

    Added SelfAttention copy

---
+++ rewrite/9.2/SelfAttention/0_copy.py
@@ -0,0 +1,223 @@
+import copy
+import numpy as np
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
+
+# from data_generation.square_sequences import generate_sequences
+from StepByStep import StepByStep
+
+
+torch.set_default_dtype(torch.float64)
+torch.manual_seed(23)
+
+
+class Attention(nn.Module):
+    def __init__(self, hidden_dim, input_dim=None, proj_values=False):
+        super().__init__()
+        self.d_k = hidden_dim
+        self.input_dim = hidden_dim if input_dim is None else input_dim
+        self.proj_values = proj_values
+        # Affine transformations for Q, K, and V
+        self.linear_query = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_key = nn.Linear(self.input_dim, hidden_dim)
+        self.linear_value = nn.Linear(self.input_dim, hidden_dim)
+        self.alphas = None
+
+    def init_keys(self, keys):
+        self.keys = keys
+        self.proj_keys = self.linear_key(self.keys)
+        self.values = self.linear_value(self.keys) \
+                      if self.proj_values else self.keys
+
+    def score_function(self, query):
+        proj_query = self.linear_query(query)
+        # scaled dot product
+        # N, 1, H x N, H, L -> N, 1, L
+        dot_products = torch.bmm(proj_query, self.proj_keys.permute(0, 2, 1))
+        scores =  dot_products / np.sqrt(self.d_k)
+        return scores
+
+    def forward(self, query, mask=None):
+        # Query is batch-first N, 1, H
+        scores = self.score_function(query) # N, 1, L
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+        alphas = F.softmax(scores, dim=-1) # N, 1, L
+        self.alphas = alphas.detach()
+
+        # N, 1, L x N, L, H -> N, 1, H
+        context = torch.bmm(alphas, self.values)
+        return context
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model, input_dim=None, proj_values=True):
+        super().__init__()
+        self.linear_out = nn.Linear(n_heads * d_model, d_model)
+        self.attn_heads = nn.ModuleList([Attention(d_model,
+                                                   input_dim=input_dim,
+                                                   proj_values=proj_values)
+                                         for _ in range(n_heads)])
+
+    def init_keys(self, key):
+        for attn in self.attn_heads:
+            attn.init_keys(key)
+
+    @property
+    def alphas(self):
+        # Shape: n_heads, N, 1, L (source)
+        return torch.stack([attn.alphas for attn in self.attn_heads], dim=0)
+
+    def output_function(self, contexts):
+        # N, 1, n_heads * D
+        concatenated = torch.cat(contexts, axis=-1)
+        # Linear transf. to go back to original dimension
+        out = self.linear_out(concatenated) # N, 1, D
+        return out
+
+    def forward(self, query, mask=None):
+        contexts = [attn(query, mask=mask) for attn in self.attn_heads]
+        out = self.output_function(contexts)
+        return out
+
+
+class EncoderSelfAttn(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None):
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.ff_units = ff_units
+        self.n_features = n_features
+        self.self_attn_heads = MultiHeadAttention(n_heads, d_model, input_dim=n_features)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, ff_units),
+            nn.ReLU(),
+            nn.Linear(ff_units, d_model),
+        )
+
+    def forward(self, query, mask=None):
+        self.self_attn_heads.init_keys(query)
+        att = self.self_attn_heads(query, mask)
+        out = self.ffn(att)
+        return out
+
+
+class DecoderSelfAttn(nn.Module):
+    def __init__(self, n_heads, d_model, ff_units, n_features=None):
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.ff_units = ff_units
+        self.n_features = d_model if n_features is None else n_features
+        self.self_attn_heads = MultiHeadAttention(n_heads, d_model, input_dim=self.n_features)
+        self.cross_attn_heads = MultiHeadAttention(n_heads, d_model)
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, ff_units),
+            nn.ReLU(),
+            nn.Linear(ff_units, self.n_features),
+        )
+
+    def init_keys(self, states):
+        self.cross_attn_heads.init_keys(states)
+
+    def forward(self, query, source_mask=None, target_mask=None):
+        self.self_attn_heads.init_keys(query)
+        att1 = self.self_attn_heads(query, target_mask)
+        att2 = self.cross_attn_heads(att1, source_mask)
+        out = self.ffn(att2)
+        return out
+
+
+class EncoderDecoderSelfAttn(nn.Module):
+    def __init__(self, encoder, decoder, input_len, target_len):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.input_len = input_len
+        self.target_len = target_len
+        self.trg_masks = self.subsequent_mask(self.target_len)
+
+    @staticmethod
+    def subsequent_mask(size):
+        attn_shape = (1, size, size)
+        subsequent_mask = (1 - torch.triu(torch.ones(attn_shape), diagonal=1))
+        return subsequent_mask
+
+    def encode(self, source_seq, source_mask):
+        # Encodes the source sequence and uses the result
+        # to initialize the decoder
+        encoder_states = self.encoder(source_seq, source_mask)
+        self.decoder.init_keys(encoder_states)
+
+    def decode(self, shifted_target_seq, source_mask=None, target_mask=None):
+        # Decodes/generates a sequence using the shifted (masked)
+        # target sequence - used in TRAIN mode
+        outputs = self.decoder(shifted_target_seq,
+                               source_mask=source_mask,
+                               target_mask=target_mask)
+        return outputs
+
+    def predict(self, source_seq, source_mask):
+        # Decodes/generates a sequence using one input
+        # at a time - used in EVAL mode
+        inputs = source_seq[:, -1:]
+        for i in range(self.target_len):
+            out = self.decode(inputs, source_mask, self.trg_masks[:, :i+1, :i+1])
+            out = torch.cat([inputs, out[:, -1:, :]], dim=-2)
+            inputs = out.detach()
+        outputs = inputs[:, 1:, :]
+        return outputs
+
+    def forward(self, X, source_mask=None):
+        # Sends the mask to the same device as the inputs
+        self.trg_masks = self.trg_masks.type_as(X).bool()
+        # Slices the input to get source sequence
+        source_seq = X[:, :self.input_len, :]
+        # Encodes source sequence AND initializes decoder
+        self.encode(source_seq, source_mask)
+        if self.training:
+            # Slices the input to get the shifted target seq
+            shifted_target_seq = X[:, self.input_len-1:-1, :]
+            # Decodes using the mask to prevent cheating
+            outputs = self.decode(shifted_target_seq, source_mask, self.trg_masks)
+        else:
+            # Decodes using its own predictions
+            outputs = self.predict(source_seq, source_mask)
+
+        return outputs
+
+
+if __name__ == "__main__":
+    encself = EncoderSelfAttn(n_heads=3, d_model=2, ff_units=10, n_features=2)
+    decself = DecoderSelfAttn(n_heads=3, d_model=2, ff_units=10, n_features=2)
+    model = EncoderDecoderSelfAttn(encself, decself, input_len=2, target_len=2)
+    loss = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+    import pickle
+    with open('random_data.pickle', 'rb') as inf:
+        data = pickle.load(inf)
+
+    points, directions = data['points'], data['directions']
+    full_train = torch.as_tensor(points).double()
+    target_train = full_train[:, 2:]
+
+    test_points, test_directions = data['test_points'], data['test_directions']
+    full_test = torch.as_tensor(test_points).double()
+    source_test = full_test[:, :2]
+    target_test = full_test[:, 2:]
+
+    train_data = TensorDataset(full_train, target_train)
+    test_data = TensorDataset(source_test, target_test)
+
+    generator = torch.Generator()
+    train_loader = DataLoader(train_data, batch_size=16, shuffle=True, generator=generator)
+    test_loader = DataLoader(test_data, batch_size=16)
+
+    sbs_seq_selfattn = StepByStep(model, loss, optimizer)
+    sbs_seq_selfattn.set_loaders(train_loader, test_loader)
+    sbs_seq_selfattn.train(20)
